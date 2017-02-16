c19065f533e0d0c24d73abddf59bab6f017d84a8
lucasboch <bochenlukasz@yahoo.com>
2017-02-16 19:07:45 +0000

    Add files via upload

---
+++ helloworld.rb
@@ -0,0 +1,17 @@
+puts "hello world"
+
+print"what is your name?"
+name=gets
+puts "Hello #{name}!"
+
+print "what is ur name?"
+
+name=gets
+if name=="lucas"
+
+  puts hello#{name!}
+
+
+else puts "you are not my creator"
+
+end
